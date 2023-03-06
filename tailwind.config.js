2b88f95abab8ab7bbd46b5e17f1bbfa90b72048c
av-erencelik <erenmemo05@gmail.com>
2023-03-06 14:45:30 +0300

    added favicons and custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        earth: {
+          50: "#fef6ec",
+          100: "#fbe6ca",
+          200: "#f7cc90",
+          300: "#f2a547",
+          400: "#f0902f",
+          500: "#e96e17",
+          600: "#ce4d11",
+          700: "#ab3312",
+          800: "#8b2915",
+          900: "#732114",
+        },
+        night: "#090A10",
+      },
+    },
   },
   plugins: [],
 };
