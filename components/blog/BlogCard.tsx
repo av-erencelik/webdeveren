b032f3b179a57d78ff4f153faf5ba72005fd5b27
av-erencelik <erenmemo05@gmail.com>
2023-03-14 00:09:59 +0300

    changed featured banners color on dark mode

--- components/blog/BlogCard.tsx
+++ components/blog/BlogCard.tsx
@@ -52,7 +52,7 @@ const BlogCard = ({ post, index }: { post: Post; index: number }) => {
         </Link>
       </div>
       {post.featured && (
-        <span className="absolute top-[15px] right-[-50px] flex h-[22px] w-[150px] rotate-45 items-end justify-center bg-earth-300 pb-[0.15rem] text-xs font-semibold tracking-wider text-earth-50">
+        <span className="absolute top-[15px] right-[-50px] flex h-[22px] w-[150px] rotate-45 items-end justify-center bg-earth-300 pb-[0.15rem] text-xs font-semibold tracking-wider text-earth-50 dark:bg-cinder-700 dark:text-earth-300">
           Featured
         </span>
       )}
