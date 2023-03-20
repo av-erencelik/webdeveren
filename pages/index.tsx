44b05538c5ff44ec686097cb72418bb51dc86c9a
av-erencelik <erenmemo05@gmail.com>
2023-03-20 14:51:17 +0300

    google vertification

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,6 +35,7 @@ export default function Home({ preview, data }: { preview: boolean; data: Post[]
         <title>Web Dev Eren</title>
         <meta name="description" content={t("meta.homeDesc") ? (t("meta.homeDesc") as string) : ""} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="google-site-verification" content="dXQ55giVy7l2I-W7sVfBXPp4X1go0DwFixGvl4hrgC8" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
